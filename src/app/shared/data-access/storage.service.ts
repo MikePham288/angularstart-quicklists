ff0973ec2e6f8e3b9e854c7406666ae3974b465e
Mike Pham <mikephse@gmail.com>
2024-01-22 05:22:13 +0700

    create storage service

---
+++ src/app/shared/data-access/storage.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, InjectionToken, PLATFORM_ID, inject } from '@angular/core';
+
+export const LOCAL_STORAGE = new InjectionToken<Storage>(
+  'window local storage object',
+  {
+    providedIn: 'root',
+    factory: () => {
+      return inject(PLATFORM_ID) === 'browser'
+        ? window.localStorage
+        : ({} as Storage);
+    },
+  }
+);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  storage = inject(LOCAL_STORAGE);
+}
