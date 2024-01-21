b8584a5a53b0fc30bcd6ec8c21466ba2d2a92f1a
Mike Pham <mikephse@gmail.com>
2024-01-22 04:36:30 +0700

    get checklist by id

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ChecklistService } from '../shared/data-access/checklist.service';
 
 @Component({
   selector: 'app-checklist',
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './checklist.component.scss'
 })
 export default class ChecklistComponent {
+  checklistService = inject(ChecklistService);
+  route = inject(ActivatedRoute);
 
+  params = toSignal(this.route.paramMap);
+
+  checklist = computed(() =>
+    this.checklistService
+      .checklists()
+      .find((checklist) => checklist.id === this.params()?.get('id'))
+  );
 }
