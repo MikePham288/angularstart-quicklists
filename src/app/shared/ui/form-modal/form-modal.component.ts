e60a0a5e35e7354fa6ac049ac9f93a6fbe2a01aa
Mike Pham <mikephse@gmail.com>
2024-01-22 03:50:32 +0700

    create the form modal component

---
+++ src/app/shared/ui/form-modal/form-modal.component.ts
@@ -0,0 +1,17 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-modal',
+  standalone: true,
+  imports: [ReactiveFormsModule, KeyValuePipe],
+  templateUrl: './form-modal.component.html',
+  styleUrl: './form-modal.component.scss'
+})
+export class FormModalComponent {
+  @Input({ required: true }) formGroup!: FormGroup;
+  @Input({ required: true }) title!: string;
+  @Output() save = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
+}
