a00fb2bc32e6b3929c95a52c722659717344c727
Mike Pham <mikephse@gmail.com>
2024-01-22 04:44:04 +0700

    navigate to checklist detail page

--- src/app/home/ui/checklist-list/checklist-list.component.ts
+++ src/app/home/ui/checklist-list/checklist-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Checklist } from '../../../shared/interfaces/checklist.interface.ts';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checklist-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './checklist-list.component.html',
   styleUrl: './checklist-list.component.scss'
 })
