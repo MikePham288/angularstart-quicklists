9c06913478e3d949da0e82061f84e01c42136b69
Mike Pham <mikephse@gmail.com>
2024-01-22 03:23:25 +0700

    create modal wrapper component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../shared/ui/modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/shared/ui/modal/modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, ContentChild, Input, TemplateRef, inject } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+dialog = inject(Dialog);
+
+  @Input() set isOpen(value: boolean) {
+    if (value) {
+      this.dialog.open(this.template, { panelClass: 'dialog-container' });
+    } else {
+      this.dialog.closeAll();
+    }
+  }
+
+  @ContentChild(TemplateRef, { static: false }) template!: TemplateRef<any>;
+}
