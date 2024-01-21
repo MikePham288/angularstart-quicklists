c9b1c2d8979597690c667500a042ce96c323e95d
Mike Pham <mikephse@gmail.com>
2024-01-22 04:00:22 +0700

    readd @angular/cdk cuz animation was not working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ModalComponent } from '../shared/ui/modal/modal.component';
 import { Checklist } from '../shared/interfaces/checklist.interface.ts';
+import { FormBuilder } from '@angular/forms';
+import { FormModalComponent } from '../shared/ui/form-modal/form-modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ModalComponent],
+  imports: [ModalComponent, FormModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export default class HomeComponent {
+  formBuilder = inject(FormBuilder);
 
   checklistBeingEdited = signal<Partial<Checklist> | null>(null);
+
+  checklistForm = this.formBuilder.nonNullable.group({
+    title: [''],
+  });
 }
